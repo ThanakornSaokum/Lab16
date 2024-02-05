b0363096afa907a1668461007577a9f99f6eb450
ThanakornSaokum <thanakorn01350@gmail.com>
2024-02-06 01:57:14 +0700

    lab16-1

--- lab16_1.cpp
+++ lab16_1.cpp
@@ -32,3 +32,42 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+
+void showData(double *x,int N,int M){
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout << x[i];
+        if(i % M == M - 1) cout << endl;
+        else cout << " ";
+    }
+}
+
+void randData(double *x,int N,int M){
+    for(int i = 0; i < N*M; i++){
+        x[i] = rand()%101/100.0;
+    }
+}
+
+void findRowSum(const double *x,double *sum,int N,int M){
+    for(int i = 0;i < N;i++){
+        *(sum + i) = 0;
+    
+        for(int j = 0;j < M;j++){
+            *(sum + i) += *(x + j + i * M); 
+            
+        }    
+    }
+    
+}
+
+void findColSum(const double *x,double *sum,int N,int M){
+    for(int i = 0;i < M;i++){
+        *(sum + i) = 0;
+    
+        for(int j = 0;j < N;j++){
+            *(sum + i) += *(x + i + j * M); 
+            
+        }    
+    }
+    
+}
